b586f1b7261554a84da8590b097a80a87b1adc77
michelcoutinhodasilva <michelcoutinho010@gmail.com>
2026-05-25 17:29:51 -0300

    aula 7

---
+++ Aula 7/scripts/script.js
@@ -0,0 +1,41 @@
+
+// const botao = document.getElementById("botaotema")
+// const corpoPagina = document.body
+// const caixa = document.getElementById("caixa")
+// const titulo =document.getElementById("titulo")
+// const text = document.getElementById("texto")
+
+// let modoEscuroAtivado = false
+// botao.addEventListener("click", () =>{
+//     if (modoEscuroAtivado === true){
+//         corpoPagina.style.backgroundColor ="#ffffff" //muda o fundo para branco
+//         corpoPagina.style.color = "#000000" // muda as letras para preto
+//         caixa.style.backgroundColor = "#ffffff" // muda cor de fundo da caixinha
+//         caixa.style.border ="1px solid #cccccc" // muda a cor da borda da caixinha
+//         botao.style.backgroundColor ="#000000" // muda cor do botão 
+//         botao.style.color = "#ffffff" // muda a cor da letra do botao
+//         botao.style.border = "none" // tira a borda do botao
+//         titulo.style.color = "#222222" // muda cor da letra do titulo
+//         text.style.color = "#222222" //muda a cor da letra do texto
+//         botao.innerText = "ativando modo escuro" // muda o texto do botão
+//         modoEscuroAtivado=false // indica que o modo escuro não esta mais ativado
+//     }
+//     else {
+//         corpoPagina.style.backgroundColor = "#1e1e1e"
+//         corpoPagina.style.color = "#ffffff"
+
+//         caixa.style.backgroundColor ="#333333"
+//         caixa.style.border ="1px solid #ffffff"
+        
+//         botao.style.backgroundColor = "#ffffff"
+//         botao.style.color = "#000000"
+//         botao.style.border = "none"
+
+//         titulo.style.color = "#00ffff"
+//         text.style.color ="#dddddd"
+
+//         botao.innerText = "ativar modo claro"
+//         modoEscuroAtivado = true
+//     }
+// })
+
