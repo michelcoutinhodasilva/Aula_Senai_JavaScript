5d2f8c649680b71975940791dd7173083be5500c
michelcoutinhodasilva <michelcoutinho010@gmail.com>
2026-05-29 15:02:40 -0300

    atividade Desafio Lanchonete (DOM + LocalStorage) da aula 10 concluido

--- Aula 10/scripts/script.js
+++ Aula 10/scripts/script.js
@@ -1,20 +1,67 @@
 const inputPedidos = document.getElementById("inputPedido");
 const botaoAdicionar = document.getElementById("btnAdicionar");
-const listaPedidos = document.getElementById("listaPedido");
+const listaPedidos = document.getElementById("listaPedidos");
+const botaoAtender = document.getElementById("btnAtender");
 
-let produtos = []
+const pedidoAtual = document.getElementById("pedidoAtual");
+let pedidos = [];
 
-function salvaPedidos(){
-    localStorage.setItem("produtos", JSON.stringify(produtos))  
+function salvaPedidos() {
+  localStorage.setItem("pedidos", JSON.stringify(pedidos));
 }
 
-function mostrarPedidos(){
-    listaPedidos.innerHTML = ""
+function mostrarPedidos() {
+  listaPedidos.innerHTML = "";
 
-    for (let i = 0; i < produtos.length; i++{
-        const li = document.createElement("li")
-        li.innerText = produtos[i]
-        
-    })
+  for (let i = 0; i < pedidos.length; i++) {
+    const li = document.createElement("li");
+    li.innerText = pedidos[i];
+    const botaoRemover = document.createElement("button");
+    botaoRemover.innerText = "🗑️";
+
+    botaoRemover.className = "botao-remover";
+    botaoRemover.addEventListener("click", () => {
+      removerPedidos(i);
+    });
+    li.appendChild(botaoRemover);
+    listaPedidos.appendChild(li);
+  }
+}
+function removerPedidos(posicaoPedido) {
+  pedidos.splice(posicaoPedido, 1);
+  salvaPedidos();
+  mostrarPedidos();
 }
 
+function adicionarPedidos() {
+  const valorPedido = inputPedidos.value;
+
+  if (valorPedido.trim() === "") {
+    alert("digite um pedido");
+    return;
+  }
+  pedidos.push(valorPedido);
+  inputPedidos.value = "";
+
+  salvaPedidos();
+
+  mostrarPedidos();
+}
+function carregarPedidos() {
+  const pedidosSalvos = localStorage.getItem("pedidos");
+  if (pedidosSalvos) {
+    pedidos = JSON.parse(pedidosSalvos);
+    mostrarPedidos();
+  }
+}
+botaoAdicionar.addEventListener("click", adicionarPedidos);
+carregarPedidos();
+
+botaoAtender.addEventListener("click", atenderPedido);
+function atenderPedido() {
+  const primeiro = pedidos[0];
+  pedidoAtual.innerText = "Atendendo " + primeiro;
+  pedidos.splice(0, 1);
+  salvaPedidos();
+  mostrarPedidos();
+}
